813cdeb22596f7d38da48e0c7d60fcaef95645d2
Jerome WANG immjs <informathemusic@gmail.com>
2024-05-20 16:22:05 +0200

    Fix multiline inputs; patch up!

--- dist/examples/websockets.js
+++ dist/examples/websockets.js
@@ -1,18 +1,59 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import { jsx as _jsx } from "react/jsx-runtime";
 import { render, Minitel } from '../index.js';
 import { WebSocketServer, createWebSocketStream } from 'ws';
-import React from 'react';
 import { Duplex } from 'stream';
 const wss = new WebSocketServer({ port: 8080 });
 function App() {
-    const [time, setTime] = React.useState(Date.now());
-    const [randomValue, setRandomValue] = React.useState(0);
-    React.useEffect(() => {
-        setInterval(() => {
-            setTime(Date.now());
-        }, 1000);
-    }, []);
-    return (_jsxs("yjoin", { children: [_jsx("xjoin", { bg: 7, fg: 0, widthAlign: "middle", children: Intl.DateTimeFormat('en-US', { timeStyle: 'medium' }).format(time) }), _jsxs("zjoin", { flexGrow: true, children: [_jsxs("yjoin", { heightAlign: "middle", gap: "space-evenly", children: [_jsxs("xjoin", { widthAlign: "middle", children: [_jsx("para", { bg: 7, fg: 0, doubleHeight: true, children: " " }), _jsx("para", { doubleHeight: true, doubleWidth: true, bg: 7, fg: 0, children: "Up @ 9600 bauds" }), _jsx("para", { bg: 7, fg: 0, doubleHeight: true, children: " " })] }), _jsx("xjoin", { children: _jsx("para", { children: App.toString() }) }), _jsx("scroll", { height: 4, overflowY: "scroll", children: _jsxs("yjoin", { widthAlign: "middle", gap: 0, children: [_jsx("input", { width: 16 }), _jsxs("para", { children: ["Hello world", '\n', "Programmed to work and not to feel", '\n', "Not even sure that it is real", '\n', "Hello world"] })] }) })] }), _jsx("yjoin", { widthAlign: "middle", heightAlign: "middle", fillChar: '\x09', children: _jsx("yjoin", { pad: [0, 1], fillChar: ' ', children: _jsx("yjoin", { pad: [3, 6], bg: 5, fg: 3, children: "Hello world!" }) }) })] })] }));
+    // c[]);
+    // return (
+    //     <yjoin>
+    //    onst [time, setTime] = React.useState(Date.now());
+    // const [randomValue, setRandomValue] = React.useState(0);
+    // React.useEffect(() => {
+    //     setInterval(() => {
+    //         setTime(Date.now());
+    //     }, 1_000);
+    // },      <xjoin bg={7} fg={0} widthAlign="middle">
+    //             {Intl.DateTimeFormat('en-US', { timeStyle: 'medium' }).format(time)}
+    //         </xjoin>
+    //         <zjoin flexGrow>
+    //             <yjoin heightAlign="middle" gap="space-evenly">
+    //                 <xjoin widthAlign="middle">
+    //                     <para bg={7} fg={0} doubleHeight> </para>
+    //                     <para doubleHeight doubleWidth bg={7} fg={0}>
+    //                         Up @ 9600 bauds
+    //                     </para>
+    //                     <para bg={7} fg={0} doubleHeight> </para>
+    //                 </xjoin>
+    //                 <xjoin>
+    //                     <para>
+    //                         {App.toString()}
+    //                     </para>
+    //                 </xjoin>
+    //                 <scroll height={4} overflowY="scroll">
+    //                     <yjoin widthAlign="middle" gap={0}>
+    //                         {/* <para doubleHeight doubleWidth fg={0} bg={7}> {'I'.repeat(randomValue)} </para> */}
+    //                         <input width={16} />
+    //                         <para>
+    //                             Hello world{'\n'}
+    //                             Programmed to work and not to feel{'\n'}
+    //                             Not even sure that it is real{'\n'}
+    //                             Hello world
+    //                         </para>
+    //                     </yjoin>
+    //                 </scroll>
+    //             </yjoin>
+    //             <yjoin widthAlign="middle" heightAlign="middle" fillChar={'\x09'}>
+    //                 <yjoin pad={[0, 1]} fillChar=' '>
+    //                     <yjoin pad={[3, 6]} bg={5} fg={3}>
+    //                         Hello world!
+    //                     </yjoin>
+    //                 </yjoin>
+    //             </yjoin>
+    //         </zjoin>
+    //     </yjoin>
+    // );
+    return _jsx("yjoin", { children: _jsx("input", { multiline: true, flexGrow: true }) });
 }
 ;
 class DuplexBridge extends Duplex {
@@ -36,7 +77,7 @@ class DuplexBridge extends Duplex {
 }
 wss.on('connection', function connection(ws) {
     const bridge = new DuplexBridge(createWebSocketStream(ws, { decodeStrings: false }), ws, { decodeStrings: false });
-    const minitel = new Minitel(bridge, { statusBar: true });
+    const minitel = new Minitel(bridge, { statusBar: false });
     ws.on('message', (data) => console.log({ data }));
     const derender = render(_jsx(App, {}), minitel);
     ws.on('close', () => {
--- src/examples/websockets.tsx
+++ src/examples/websockets.tsx
@@ -7,55 +7,56 @@ import { Duplex, DuplexOptions } from 'stream';
 const wss = new WebSocketServer({ port: 8080 });
 
 function App() {
-    const [time, setTime] = React.useState(Date.now());
-    const [randomValue, setRandomValue] = React.useState(0);
-    React.useEffect(() => {
-        setInterval(() => {
-            setTime(Date.now());
-        }, 1_000);
-    }, []);
-    return (
-        <yjoin>
-            <xjoin bg={7} fg={0} widthAlign="middle">
-                {Intl.DateTimeFormat('en-US', { timeStyle: 'medium' }).format(time)}
-            </xjoin>
-            <zjoin flexGrow>
-                <yjoin heightAlign="middle" gap="space-evenly">
-                    <xjoin widthAlign="middle">
-                        <para bg={7} fg={0} doubleHeight> </para>
-                        <para doubleHeight doubleWidth bg={7} fg={0}>
-                            Up @ 9600 bauds
-                        </para>
-                        <para bg={7} fg={0} doubleHeight> </para>
-                    </xjoin>
-                    <xjoin>
-                        <para>
-                            {App.toString()}
-                        </para>
-                    </xjoin>
-                    <scroll height={4} overflowY="scroll">
-                        <yjoin widthAlign="middle" gap={0}>
-                            {/* <para doubleHeight doubleWidth fg={0} bg={7}> {'I'.repeat(randomValue)} </para> */}
-                            <input width={16} />
-                            <para>
-                                Hello world{'\n'}
-                                Programmed to work and not to feel{'\n'}
-                                Not even sure that it is real{'\n'}
-                                Hello world
-                            </para>
-                        </yjoin>
-                    </scroll>
-                </yjoin>
-                <yjoin widthAlign="middle" heightAlign="middle" fillChar={'\x09'}>
-                    <yjoin pad={[0, 1]} fillChar=' '>
-                        <yjoin pad={[3, 6]} bg={5} fg={3}>
-                            Hello world!
-                        </yjoin>
-                    </yjoin>
-                </yjoin>
-            </zjoin>
-        </yjoin>
-    );
+    // c[]);
+    // return (
+    //     <yjoin>
+    //    onst [time, setTime] = React.useState(Date.now());
+    // const [randomValue, setRandomValue] = React.useState(0);
+    // React.useEffect(() => {
+    //     setInterval(() => {
+    //         setTime(Date.now());
+    //     }, 1_000);
+    // },      <xjoin bg={7} fg={0} widthAlign="middle">
+    //             {Intl.DateTimeFormat('en-US', { timeStyle: 'medium' }).format(time)}
+    //         </xjoin>
+    //         <zjoin flexGrow>
+    //             <yjoin heightAlign="middle" gap="space-evenly">
+    //                 <xjoin widthAlign="middle">
+    //                     <para bg={7} fg={0} doubleHeight> </para>
+    //                     <para doubleHeight doubleWidth bg={7} fg={0}>
+    //                         Up @ 9600 bauds
+    //                     </para>
+    //                     <para bg={7} fg={0} doubleHeight> </para>
+    //                 </xjoin>
+    //                 <xjoin>
+    //                     <para>
+    //                         {App.toString()}
+    //                     </para>
+    //                 </xjoin>
+    //                 <scroll height={4} overflowY="scroll">
+    //                     <yjoin widthAlign="middle" gap={0}>
+    //                         {/* <para doubleHeight doubleWidth fg={0} bg={7}> {'I'.repeat(randomValue)} </para> */}
+    //                         <input width={16} />
+    //                         <para>
+    //                             Hello world{'\n'}
+    //                             Programmed to work and not to feel{'\n'}
+    //                             Not even sure that it is real{'\n'}
+    //                             Hello world
+    //                         </para>
+    //                     </yjoin>
+    //                 </scroll>
+    //             </yjoin>
+    //             <yjoin widthAlign="middle" heightAlign="middle" fillChar={'\x09'}>
+    //                 <yjoin pad={[0, 1]} fillChar=' '>
+    //                     <yjoin pad={[3, 6]} bg={5} fg={3}>
+    //                         Hello world!
+    //                     </yjoin>
+    //                 </yjoin>
+    //             </yjoin>
+    //         </zjoin>
+    //     </yjoin>
+    // );
+    return <yjoin><input multiline flexGrow /></yjoin>
 };
 
 class DuplexBridge extends Duplex { // not nice making me do this, couldve been prevented with a simple event before CLOSING ready state
@@ -82,7 +83,7 @@ class DuplexBridge extends Duplex { // not nice making me do this, couldve been 
 wss.on('connection', function connection(ws) {
     const bridge = new DuplexBridge(createWebSocketStream(ws, { decodeStrings: false }), ws, { decodeStrings: false })
 
-    const minitel = new Minitel(bridge, { statusBar: true });
+    const minitel = new Minitel(bridge, { statusBar: false });
 
     ws.on('message', (data) => console.log({ data }));
 
